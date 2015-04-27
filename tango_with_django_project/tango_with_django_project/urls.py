fbd5d0e948f4a95483b058100f246d05ddfa5765
Luke Merrett <lukeamerrett@gmail.com>
2015-04-27 13:52:33 +0100

    Added a home project to test mapping urls further

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^rango/', include('rango.urls'))
+    url(r'^rango/', include('rango.urls')),
+    url(r'^$', include('home.urls'))
 ]
