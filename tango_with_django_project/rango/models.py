28766a280084904389758e3198a6b57ade9577f5
Luke Merrett <lukeamerrett@gmail.com>
2015-04-29 16:26:33 +0100

    Added a 'likes' column to the Category model

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    likes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
