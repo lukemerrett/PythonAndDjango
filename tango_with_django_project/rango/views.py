766f004b7ff1e667d4b7674d632a5e79dbc41a87
Luke Merrett <lukeamerrett@gmail.com>
2015-04-29 17:09:14 +0100

    Added the ability to view a Category and it's associated pages using the slug

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'categories': category_list}
 
     return render(request, 'rango/index.html', context_dict)
+
+def category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        # We get here if we didn't find the specified category.
+        # Don't do anything - the template displays the "no category" message for us.
+        pass
+
+    return render(request, 'rango/category.html', context_dict)
